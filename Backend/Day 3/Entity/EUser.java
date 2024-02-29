c8a772beb21cf9d2500319dcf2c5a4e08781b6f9
hariniwinter <113656649+hariniwinter@users.noreply.github.com>
2024-02-29 16:10:14 +0530

    Add files via upload

---
+++ Backend/Day 3/Entity/EUser.java
@@ -0,0 +1,22 @@
+package com.example.eventmanagement.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+public class EUser {
+
+       @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    
+}
