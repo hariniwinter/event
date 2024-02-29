714636195e5d9094d7bd9e4b9a0bd5964abfb2b7
hariniwinter <113656649+hariniwinter@users.noreply.github.com>
2024-02-29 16:13:36 +0530

    Delete Backend/Day 6/src directory

--- Backend/Day 6/src/main/java/com/example/eventmanagement/Repository/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.eventmanagement.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.eventmanagement.Entity.Admin;
-
-public interface AdminRepo extends JpaRepository<Admin,Integer> {
-    
-}
--- Backend/Day 6/src/main/java/com/example/eventmanagement/Repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.eventmanagement.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.eventmanagement.Entity.EUser;
-
-public interface UserRepo extends JpaRepository<EUser,Integer>{
-    
-}
