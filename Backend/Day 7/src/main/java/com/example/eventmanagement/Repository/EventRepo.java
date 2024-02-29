5b68c97d9aba3f88ecdb8c5fbe5dd0296f6ca468
hariniwinter <113656649+hariniwinter@users.noreply.github.com>
2024-02-29 16:13:49 +0530

    Delete Backend/Day 7/src directory

--- Backend/Day 7/src/main/java/com/example/eventmanagement/Repository/EventRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.eventmanagement.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.eventmanagement.Entity.Event;
-
-public interface EventRepo extends JpaRepository<Event,Integer>{
-    
-}
--- Backend/Day 7/src/main/java/com/example/eventmanagement/Service/EventService.java
+++
@@ -1,7 +0,0 @@
-package com.example.eventmanagement.Service;
-
-import com.example.eventmanagement.DTO.EventDTO;
-
-public interface EventService {
-    void createEvent(EventDTO eventdto);
-}
