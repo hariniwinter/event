4e3bfee3e9939dd7d063af9b9882b099c1305da3
hariniwinter <113656649+hariniwinter@users.noreply.github.com>
2024-02-29 14:40:25 +0530

    Add files via upload

---
+++ Backend/Day 7/src/main/java/com/example/eventmanagement/Service/ServiceImplementation/EventServiceImplementation.java
@@ -0,0 +1,35 @@
+package com.example.eventmanagement.Service.ServiceImplementation;
+
+import org.springframework.stereotype.Service;
+
+import com.example.eventmanagement.DTO.EventDTO;
+import com.example.eventmanagement.Entity.Event;
+import com.example.eventmanagement.Repository.EventRepo;
+import com.example.eventmanagement.Service.EventService;
+
+import lombok.AllArgsConstructor;
+
+
+@Service
+@AllArgsConstructor
+public class EventServiceImplementation implements EventService {
+    private EventRepo eventRepo;
+
+    @Override
+    public void createEvent(EventDTO eventdto)
+    {
+        Event event = new Event();
+        event.setEName(eventdto.getEName());
+        event.setEVenue(eventdto.getEVenue());
+        event.setEdate(eventdto.getEdate());
+        event.setECost(eventdto.getECost());
+        eventRepo.save(event);
+
+
+    }
+    
+
+    
+
+    
+}
